eace8a67da5400b2e495a0bc634bdd312cda8cf5
Jiasheng Lu <jonsnowljs@gmail.com>
2021-12-20 17:49:29 -0700

    Calculate most used language

--- src/components/Charts/Pie3D.js
+++ src/components/Charts/Pie3D.js
@@ -1,7 +1,44 @@
+// STEP 1 - Include Dependencies
+// Include react
 import React from 'react';
 
-const Pie3D = () => {
-  return <div>chart</div>;
-};
+// Include the react-fusioncharts component
+import ReactFC from 'react-fusioncharts';
 
-export default Pie3D;
+// Include the fusioncharts library
+import FusionCharts from 'fusioncharts';
+
+// Include the chart type
+import Column from 'fusioncharts/fusioncharts.charts';
+
+// Include the theme as fusion
+import FusionTheme from 'fusioncharts/themes/fusioncharts.theme.fusion';
+
+// Adding the chart and theme as dependency to the core fusioncharts
+ReactFC.fcRoot(FusionCharts, Column, FusionTheme);
+
+// STEP 4 - Creating the DOM element to pass the react-fusioncharts component
+const ChartComponent = ({ data }) => {
+  // STEP 3 - Creating the JSON object to store the chart configurations
+  const chartConfigs = {
+    type: 'pie2d', // The chart type
+    width: '400', // Width of the chart
+    height: '400', // Height of the chart
+    dataFormat: 'json', // Data type
+    dataSource: {
+      // Chart Configuration
+      chart: {
+        //Set the chart caption
+        caption: 'Languages',
+        theme: 'fusion',
+        decimals: 0,
+        pieRadius: '45%',
+        theme: 'fusion',
+      },
+      // Chart Data
+      data,
+    },
+  };
+  return <ReactFC {...chartConfigs} />;
+};
+export default ChartComponent;
--- src/components/Repos.js
+++ src/components/Repos.js
@@ -1,9 +1,53 @@
-import React from 'react';
+import React, { useContext } from 'react';
 import styled from 'styled-components';
 import { GithubContext } from '../context/context';
 import { ExampleChart, Pie3D, Column3D, Bar3D, Doughnut2D } from './Charts';
 const Repos = () => {
-  return <h2>repos component</h2>;
+  const { repos } = useContext(GithubContext);
+
+  let languages = repos.reduce((total, item) => {
+    console.log(total)
+    console.log(item)
+    const {language} = item;
+    if (!language) return total;
+
+    if (!total[language]) {
+      total[language]  = {label:language, value:1}
+    } else {
+      total[language] = {...total[language], value: total[language].value + 1}
+    }
+    return total;
+
+  }, {})
+
+  console.log(languages)
+  languages = Object.values(languages).sort((a, b) => {
+    return b.value - a.value;
+  }).slice(0, 5)
+  console.log(languages)
+
+  const chartData = [
+    {
+      label: 'HTML',
+      value: '13',
+    },
+    {
+      label: 'CSS',
+      value: '300',
+    },
+    {
+      label: 'JavaScript',
+      value: '80',
+    },
+  ];
+
+  return (
+    <section className="section">
+      <Wrapper className='section-center'>
+        <Pie3D data={languages} />
+      </Wrapper>
+    </section>
+  );
 };
 
 const Wrapper = styled.div`
